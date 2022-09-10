8c7d1b184e65cf6c2bf0748da12ade9b9d393a7e
Mehir_007 <mehirrouth20000@gmail.com>
2022-09-10 18:01:44 +0530

    Binary Search

--- Binary_Search.cpp
+++ Binary_Search.cpp
@@ -3,10 +3,21 @@ using namespace std;
 
 int Binary_Search(int arr[],int n,int x)
 {
-   
+   int l=0,h=n-1;
+   while(l<=h)
+   {
+     int mid=l+(h-l)/2;
+     if(arr[mid]==x)
+     return mid;
+     else if(arr[mid]<x)
+     l=mid+1;
+     else
+     h=mid-1;
+   }
+   return -1;
 }
 int main(){
     int arr[]={1,2,3,4,5,6,7,8,9,10};
     int n=sizeof(arr)/sizeof(arr[0]);
-    cout<<Binary_Search(arr,n,6);
+    cout<<Binary_Search(arr,n,3);
 }

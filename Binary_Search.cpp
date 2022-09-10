a601ce009703a0b886d1d71b37595419d93cce6a
Mehir_007 <mehirrouth20000@gmail.com>
2022-09-10 16:24:25 +0530

    Search Algo

---
+++ Binary_Search.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int Binary_Search(int arr[],int n,int x)
+{
+   
+}
+int main(){
+    int arr[]={1,2,3,4,5,6,7,8,9,10};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    cout<<Binary_Search(arr,n,6);
+}
